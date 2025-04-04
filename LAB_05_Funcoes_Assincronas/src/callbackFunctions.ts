772c7372b943e0db3d2525ae9b59a182b369737b
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2025-04-04 20:20:38 -0300

    Inicio Lab 5

---
+++ LAB_05_Funcoes_Assincronas/src/callbackFunctions.ts
@@ -0,0 +1,17 @@
+function concatenar ( a : string , b : string, callback:( param : string ) => void) : void {
+    var op = a + " " + b ;
+    callback ( op );
+}
+
+function somar ( a : number , b : number, callback:( param : number ) => void) : void {
+    var op = a + b;
+    callback ( op );
+}
+
+function imprimir ( value : any ) : void {
+    console.log("A operacao efetuada resultou em: " + value);
+}
+
+concatenar("Hello", "World", imprimir);
+somar (2 , 2, imprimir ) ;
+
