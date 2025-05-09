39e78cf5989838e02cf7ee3ddf99cc5893e13365
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2025-05-09 20:42:27 -0300

    Feat: Inicio criação api express

---
+++ LAB_13/src/Fabricante.ts
@@ -0,0 +1,10 @@
+import { Endereco } from "./Endereco";
+export class Fabricante{
+    nome : string
+    endereco : Endereco
+
+    constructor(nome : string, endereco: Endereco){
+        this.nome = nome
+        this.endereco = endereco
+    }
+}
---
+++ LAB_13/src/app.ts
@@ -0,0 +1,33 @@
+import express, {Request, Response} from "express"
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+app.use(express.json())
+
+function helloWorld(req: Request, res: Response): void{
+    res.status(200).send('Hello World!!!')
+}
+
+function filtrarProdutoPorId(req: Request, res: Response):void{
+    try{
+        let id = req.params.id
+        res.status(200).json(
+            {
+                ID: id
+            }
+        )
+    }catch(e: unknown){
+        res.status(400).json(
+            {
+                Message: "Necessário informar ID"
+            }
+    )
+    }
+}
+
+
+// function filtrarProdutoPorNome()
+
+app.get('/api/hello', helloWorld)
+
+app.listen(PORT, ()=> console.log(`API em execução no URL: http://localhost:${PORT}`))
